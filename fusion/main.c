fcda141dd47e35fb13dc12796c2fb7c8bc769f0c
Jeremie Miserez <jeremie@miserez.org>
2014-03-24 08:15:41 +0100

    read and write TIFF (.tif) files using libtiff

---
+++ fusion/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <tiffio.h>
+
+int main(void){
+    TIFF* tif = TIFFOpen("gradient.tif", "r");
+
+    if (tif) {
+        uint32 w, h;
+        size_t npixels;
+        uint32* raster;
+
+        TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
+        TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
+        npixels = w * h;
+        printf("w=%d h=%d\n",w,h);
+        raster = (uint32*) _TIFFmalloc(npixels * sizeof (uint32));
+        if (raster != NULL) {
+            if (TIFFReadRGBAImageOriented(tif, w, h, raster, ORIENTATION_TOPLEFT, 0)) { //same as GIMP and others
+                for(int i = 0; i < npixels; i++){
+                    unsigned char r = TIFFGetR(raster[i]);
+                    unsigned char g = TIFFGetG(raster[i]);
+                    unsigned char b = TIFFGetB(raster[i]);
+                    unsigned char a = TIFFGetA(raster[i]);
+                    printf("[%u %u %u %u] ", r,g,b,a);
+                    if(i%w == w-1){
+                        printf("\n");
+                    }
+                }
+            }
+            _TIFFfree(raster);
+        }
+        TIFFClose(tif);
+        TIFF *out= TIFFOpen("new.tif", "w");
+        int samplesperpixel = 4;
+        int width = 256;
+        int height = 256;
+        char image[width*height*samplesperpixel];
+        for(int i = 0; i < 256; i++){
+            for(int k = 0; k < 256*4; k+=4){
+                image[i*256*4+k+0] = ((i+k)/6+k-i) % 256; //R
+                image[i*256*4+k+1] = ((i+k)/4+i-k) % 256; //G
+                image[i*256*4+k+2] = (i+k)/2+k % 256;  //B
+                image[i*256*4+k+3] = 255; //A
+            }
+        }
+        TIFFSetField(out, TIFFTAG_IMAGEWIDTH, width);
+        TIFFSetField(out, TIFFTAG_IMAGELENGTH, height);
+        TIFFSetField(out, TIFFTAG_SAMPLESPERPIXEL, samplesperpixel);
+        TIFFSetField(out, TIFFTAG_BITSPERSAMPLE, 8);
+        TIFFSetField(out, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
+        TIFFSetField(out, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
+        TIFFSetField(out, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_RGB);
+
+        TIFFSetField(out, TIFFTAG_ROWSPERSTRIP, height);
+        TIFFWriteEncodedStrip(out, 0, &image[0], width*height*samplesperpixel);
+        TIFFClose(out);
+    }
+    return 0;
+}
