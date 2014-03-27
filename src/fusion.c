15a2877bd61131a4bcc25feff741b699c081c002
Jeremie Miserez <jeremie@miserez.org>
2014-03-27 08:33:54 +0100

    contrast function, convolution, matlab helper functions

--- src/fusion.c
+++ src/fusion.c
@@ -14,10 +14,22 @@
 int main(int argc, char *argv[]);
 void run(uint32_t **images, uint32_t nimages, uint32_t width, uint32_t height);
 
-void rgb2gray(double *rgb, size_t npixels, double* ret_gray);
-void ones(double *arr, size_t len);
+void rgb2gray(double *im, size_t npixels, double* dst);
+void ones(double *dst, size_t len);
+void ones_foreach(double **dst, size_t len, uint32_t N);
+void zeros(double *dst, size_t len);
+void zeros_foreach(double **dst, size_t len, uint32_t N);
+void scalar_mult(double *src, size_t src_len, double val, double *dst);
+void scalar_pow(double *src, size_t src_len, double val, double *dst);
+void elementwise_mult(double *src1, size_t src1_len, double *src2, double *dst);
+void scalar_abs(double *src, size_t src_len, double *dst);
+void conv3x3_mono_replicate(double* mono, uint32_t r, uint32_t c, double* h, double* dst);
 
 void exposure_fusion(double** I, int r, int c, int N, double m[3], double* R);
+void contrast(double *im, uint32_t r, uint32_t c, double *C);
+
+void malloc_foreach(double **dst, size_t size, uint32_t N);
+
 void load_images(char **path, int nimages, uint32_t **ret_stack, uint32_t *ret_widths, uint32_t *ret_heights);
 void store_image(char* path, double *R, uint32_t height, uint32_t width);
 
@@ -108,6 +120,7 @@ void run(uint32_t **images, uint32_t nimages, uint32_t width, uint32_t height){
 
         tiff2rgb(images[i], npixels, converted_image);
         I[i] = converted_image;
+        scalar_mult(I[i],npixels*3,1.0/255.0,I[i]);
     }
 
     // malloc space for the fused image
@@ -124,7 +137,7 @@ void run(uint32_t **images, uint32_t nimages, uint32_t width, uint32_t height){
     //run fusion
     exposure_fusion(I, height, width, nimages, m, R);
 
-    store_image("output.tif", R, height, width);
+    store_image("debug.tif", R, height, width);
 
 }
 
@@ -144,29 +157,67 @@ void run(uint32_t **images, uint32_t nimages, uint32_t width, uint32_t height){
  *        respectively.
  */
 void exposure_fusion(double** I, int r, int c, int N, double m[3], double* R){
-    uint32_t width = c;
-    uint32_t height = r;
-    uint32_t npixels = r*c;
+    size_t I_len = N;
+    size_t I_len2 = r*c*3;
 
-    size_t W_len = r*c*N;
-    double *W = malloc(W_len*sizeof(double));
-    assert(W != NULL);
-    ones(W, W_len);
-    //TODO
+    double contrast_parm = m[0];
+    double sat_parm = m[1];
+    double wexp_parm = m[2];
 
+    for (int n = 0; n < N; n++){
+        size_t W_len = r*c; //1 value/pixel
+        double* W = malloc(W_len*sizeof(double));
+        assert(W != NULL);
+        ones(W, W_len);
 
-    for(int i = 0; i < npixels*3; i++){
-        R[i] = I[0][i];
+        if(contrast_parm > 0){
+            size_t C_len = r*c; //1 value/pixel
+
+            double* C = malloc(C_len*sizeof(double));
+            assert(C != NULL);
+
+            contrast(I[n],r,c,C);
+            scalar_pow(C,C_len,contrast_parm,C);
+
+            assert(W_len == C_len);
+            elementwise_mult(W,W_len,C,W);
+        }
+
+    }
+
+    //TODO: remove this and replace with fused image
+    for(int i = 0; i < I_len2; i++){
+        R[i] = I[1][i]; //just copy
     }
     printf("done\n");
 }
 
+void contrast(double *im, uint32_t r, uint32_t c, double *C){
+    //laplacian filter
+    double h[] = {
+        0.0, 1.0, 0.0,
+        1.0, -4.0, 1.0,
+        0.0, 1.0, 0.0
+    };
+    zeros(C, r*c);
+    //for each image, calculate contrast measure on grayscale version of the image
+    size_t mono_len = r*c; //1 value/pixel
+    double *mono = malloc(mono_len*sizeof(double));
+    assert(mono != NULL);
+    rgb2gray(im, r*c, mono);
+    conv3x3_mono_replicate(mono,r,c,h,C);
+}
 
 //
 // Helper functions
 //
 
-
+void malloc_foreach(double **dst, size_t size, uint32_t N){
+    for(int i = 0; i < N; i++){
+        dst[i] = malloc(size);
+        assert(dst[i] != NULL);
+    }
+}
 
 //
 // MATLAB-equivalent functionality
@@ -181,19 +232,124 @@ void exposure_fusion(double** I, int r, int c, int N, double m[3], double* R){
  * @param npixels Size of image in pixels
  * @param gray (out) Output image
  */
-void rgb2gray(double *rgb, size_t npixels, double* ret_gray){
+void rgb2gray(double *im, size_t npixels, double* dst){
     for(int i = 0; i < npixels; i++){
-        double r = rgb[i*3];
-        double g = rgb[i*3+1];
-        double b = rgb[i*3+2];
-        ret_gray[i] = 0.2989 * r + 0.5870 * g + 0.1140 * b; //retain luminance, discard hue and saturation
+        double r = im[i*3];
+        double g = im[i*3+1];
+        double b = im[i*3+2];
+        dst[i] = 0.2989 * r + 0.5870 * g + 0.1140 * b; //retain luminance, discard hue and saturation
     }
 }
 
-void ones(double *arr, size_t len){
+void ones(double *dst, size_t len){
     for(int i = 0; i < len; i++){
-        arr[i] = (double)1.0;
+        dst[i] = (double)1.0;
     }
+}
+void ones_foreach(double **dst, size_t len, uint32_t N){
+    for(int i = 0; i < N; i++){
+        ones(dst[i], len);
+    }
+}
+
+void zeros(double *dst, size_t len){
+    for(int i = 0; i < len; i++){
+        dst[i] = (double)0.0;
+    }
+}
+void zeros_foreach(double **dst, size_t len, uint32_t N){
+    for(int i = 0; i < N; i++){
+        zeros(dst[i],len);
+    }
+}
+
+void scalar_mult(double *src, size_t src_len, double val, double *dst){
+    for(int i = 0; i < src_len; i++){
+        dst[i] = src[i] * val;
+    }
+}
+
+void scalar_pow(double *src, size_t src_len, double val, double *dst){
+    for(int i = 0; i < src_len; i++){
+        dst[i] = pow(src[i],val);
+    }
+}
+
+void elementwise_mult(double *src1, size_t src1_len, double *src2, double *dst){
+    for(int i = 0; i < src1_len; i++){
+        dst[i] = src1[i] * src2[i];
+    }
+}
+
+void scalar_abs(double *src, size_t src_len, double *dst){
+    for(int i = 0; i < src_len; i++){
+        dst[i] = fabs(src[i]);
+    }
+}
+
+/**
+ * @brief convolution with border replication
+ */
+void conv3x3_mono_replicate(double* mono, uint32_t r, uint32_t c, double* h, double* dst){
+    for(int i = 1; i < r-1; i++){
+        for(int j = 1; j < c-1; j++){
+            dst[i*c+j] =
+                    dst[(i-1)*c+(j-1)]*h[0] + dst[(i-1)*c+(j)]*h[1] + dst[(i-1)*c+(j+1)]*h[1] +
+                    dst[(i)  *c+(j-1)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j+1)]*h[4] +
+                    dst[(i+1)*c+(j-1)]*h[5] + dst[(i+1)*c+(j)]*h[6] + dst[(i+1)*c+(j+1)]*h[7];
+        }
+    }
+    //edges
+    for(int i = 1; i < r-1; i++){
+        int j = 0;
+        dst[i*c+j] =
+                dst[(i-1)*c+(j)]*h[0] + dst[(i-1)*c+(j)]*h[1] + dst[(i-1)*c+(j+1)]*h[1] +
+                dst[(i)  *c+(j)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j+1)]*h[4] +
+                dst[(i+1)*c+(j)]*h[5] + dst[(i+1)*c+(j)]*h[6] + dst[(i+1)*c+(j+1)]*h[7];
+        j = c-1;
+        dst[i*c+j] =
+                dst[(i-1)*c+(j-1)]*h[0] + dst[(i-1)*c+(j)]*h[1] + dst[(i-1)*c+(j)]*h[1] +
+                dst[(i)  *c+(j-1)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j)]*h[4] +
+                dst[(i+1)*c+(j-1)]*h[5] + dst[(i+1)*c+(j)]*h[6] + dst[(i+1)*c+(j)]*h[7];
+    }
+    for(int j = 1; j < c-1; j++){
+        int i = 0;
+        dst[i*c+j] =
+                dst[(i)  *c+(j-1)]*h[0] + dst[(i)  *c+(j)]*h[1] + dst[(i)  *c+(j+1)]*h[1] +
+                dst[(i)  *c+(j-1)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j+1)]*h[4] +
+                dst[(i+1)*c+(j-1)]*h[5] + dst[(i+1)*c+(j)]*h[6] + dst[(i+1)*c+(j+1)]*h[7];
+        i = r-1;
+        dst[i*c+j] =
+                dst[(i-1)*c+(j-1)]*h[0] + dst[(i-1)*c+(j)]*h[1] + dst[(i-1)*c+(j+1)]*h[1] +
+                dst[(i)  *c+(j-1)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j+1)]*h[4] +
+                dst[(i)  *c+(j-1)]*h[5] + dst[(i)  *c+(j)]*h[6] + dst[(i)  *c+(j+1)]*h[7];
+    }
+    //corners
+    int i = 0;
+    int j = 0;
+    dst[i*c+j] =
+            dst[(i)*c+(j)]*h[0] + dst[(i)*c+(j)]*h[1] + dst[(i)*c+(j+1)]*h[1] +
+            dst[(i)  *c+(j)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j+1)]*h[4] +
+            dst[(i+1)*c+(j)]*h[5] + dst[(i+1)*c+(j)]*h[6] + dst[(i+1)*c+(j+1)]*h[7];
+    i = 0;
+    j = c-1;
+    dst[i*c+j] =
+            dst[(i)  *c+(j-1)]*h[0] + dst[(i)  *c+(j)]*h[1] + dst[(i)  *c+(j)]*h[1] +
+            dst[(i)  *c+(j-1)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j)]*h[4] +
+            dst[(i+1)*c+(j-1)]*h[5] + dst[(i+1)*c+(j)]*h[6] + dst[(i+1)*c+(j)]*h[7];
+    i = r-1;
+    j = 0;
+    dst[i*c+j] =
+            dst[(i-1)*c+(j-1)]*h[0] + dst[(i-1)*c+(j)]*h[1] + dst[(i-1)*c+(j+1)]*h[1] +
+            dst[(i)  *c+(j-1)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j+1)]*h[4] +
+            dst[(i)  *c+(j-1)]*h[5] + dst[(i)  *c+(j)]*h[6] + dst[(i)  *c+(j+1)]*h[7];
+    i = r-1;
+    j = c-1;
+    dst[i*c+j] =
+            dst[(i-1)*c+(j-1)]*h[0] + dst[(i-1)*c+(j)]*h[1] + dst[(i-1)*c+(j)]*h[1] +
+            dst[(i)  *c+(j-1)]*h[2] + dst[(i)  *c+(j)]*h[3] + dst[(i)  *c+(j)]*h[4] +
+            dst[(i)  *c+(j-1)]*h[5] + dst[(i)  *c+(j)]*h[6] + dst[(i)  *c+(j)]*h[7];
+
 }
 
 //
@@ -246,9 +402,9 @@ void store_image(char* path, double *R, uint32_t height, uint32_t width){
     assert(raster != NULL);
     for(int i = 0; i < npixels; i++){
         uint32_t packed = 0;
-        packed |= (uint32_t)(fmin(fmax(0.0, round(R[i*3])), 255.0));
-        packed |= ((uint32_t)(fmin(fmax(0.0, round(R[i*3+1])), 255.0))) << 8;
-        packed |= ((uint32_t)(fmin(fmax(0.0, round(R[i*3+2])), 255.0))) << 16;
+        packed |= (uint32_t)(fmin(fmax(0.0, round(R[i*3]*255.0)), 255.0));
+        packed |= ((uint32_t)(fmin(fmax(0.0, round(R[i*3+1]*255.0)), 255.0))) << 8;
+        packed |= ((uint32_t)(fmin(fmax(0.0, round(R[i*3+2]*255.0)), 255.0))) << 16;
         packed |= ((uint32_t)255) << 24;
         raster[i] = packed;
     }
