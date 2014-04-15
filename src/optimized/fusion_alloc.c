4abae97f3d6ae6e55199d56e55664f08cc7a40a3
Stefan Dietiker <stefand@ethz.ch>
2014-04-15 22:08:47 +0200

    added fusion_alloc function

--- src/optimized/fusion_alloc.c
+++ src/optimized/fusion_alloc.c
@@ -1,1 +1,13 @@
+#include "fusion.h"
 
+/**
+ * @brief alloc_fusion
+ * @param r
+ * @param c
+ * @param N
+ * @param segments
+ * @return
+ */
+int alloc_fusion(int r, int c, int N, fusion_segments_t *segments) {
+    return FUSION_ALLOC_SUCCESS;
+}
