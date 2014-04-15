eade5a93bec70255866a66ccfbcca601b202beff
Stefan Dietiker <stefand@ethz.ch>
2014-04-15 22:04:31 +0200

    few minor corrections

--- src/optimized/fusion.c
+++ src/optimized/fusion.c
@@ -1,1 +1,14 @@
+#include "fusion.h"
 
+/**
+ * @brief exposure_fusion
+ * @param I
+ * @param r
+ * @param c
+ * @param N
+ * @param m
+ * @param R
+ * @param segments
+ */
+void exposure_fusion(double** I, int r, int c, int N, double m[3], double* R, fusion_segments_t* segments) {
+}
