b2eb7b787a45247c0d279bfd9aabd93981d5a2f2
Stefan Dietiker <stefand@ethz.ch>
2014-04-17 17:28:30 +0200

    i/o function for image processing

--- src/driver/image_io.h
+++ src/driver/image_io.h
@@ -1,4 +1,9 @@
 #ifndef IMAGE_IO_H
 #define IMAGE_IO_H
 
+void load_tiff_rgb( double* rgb_image, uint32_t* r_width,
+                    uint32_t* r_height, const char* path ) {
+
+}
+
 #endif // IMAGE_IO_H
